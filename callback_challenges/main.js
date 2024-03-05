612de166acddbd7e4d0bcf33b64e0b0c7eda5b6c
Woola Johnson <woolajohnson@gmail.com>
2024-03-05 23:01:24 +0800

    Done foreach Callback Challenges

--- callback_challenges/main.js
+++ callback_challenges/main.js
@@ -1,5 +1,42 @@
 //1
-let result = foreach([1, 2, 3, 4, 5], function (num) {
-    return num * 2;
+// function foreach(arr, value) {
+//     let newArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         newArr.push(value(arr[i]));
+//     }
+//     return newArr;
+// }
+// let result = foreach([1, 2, 3, 4, 5], function (num) {
+//     return num * 2;
+// });
+// console.log(result); //this should log [2,4,6,8,10]
+
+//2
+// function foreach(arrays, callback) {
+//     let newArr = [];
+//     for (let i = 0; i < arrays.length; i++) {
+//         newArr.push(callback(arrays[i]));
+//     }
+//     return newArr;
+// }
+// result = foreach([1, 2, 3, "v88", "training"], function (val) {
+//     if (typeof val === "number") {
+//         return 0;
+//     } else {
+//         return val;
+//     }
+// });
+// console.log(result); //this should log [0,0,0,"v88","training"];
+
+//3
+function foreach(arrays, callback) {
+    let newArr = [];
+    for (let i = 0; i < arrays.length; i++) {
+        newArr.push(callback(arrays[i]));
+    }
+    return newArr;
+}
+result = foreach([1, 2, 3, "hello"], function (val) {
+    return typeof val;
 });
-console.log(result); //this should log [2,4,6,8,10]
+console.log(result); //this should log ["number", "number", "number", "string"];
