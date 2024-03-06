8e3358c31de51bab326e9bee158fd98ae10095bb
Woola Johnson <woolajohnson@gmail.com>
2024-03-06 23:17:58 +0800

    Initial Ninja Battle

---
+++ ninja_battle/ninja.js
@@ -0,0 +1,14 @@
+var ninja1 = {
+    hp: 100,
+    strength: 15,
+    attack: function () {
+        //your code
+    },
+};
+var ninja2 = {
+    hp: 150,
+    strength: 10,
+    attack: function () {
+        //your code
+    },
+};
