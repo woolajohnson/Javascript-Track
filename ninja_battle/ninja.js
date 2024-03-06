82a74da4c0532844be1bf9bd4da38534661f6002
Woola Johnson <woolajohnson@gmail.com>
2024-03-06 23:57:48 +0800

    Done logic Ninja Battle

--- ninja_battle/ninja.js
+++ ninja_battle/ninja.js
@@ -2,13 +2,41 @@ var ninja1 = {
     hp: 100,
     strength: 15,
     attack: function () {
-        //your code
+        return Math.floor(Math.random() * 16);
     },
 };
 var ninja2 = {
     hp: 150,
     strength: 10,
     attack: function () {
-        //your code
+        return Math.floor(Math.random() * 11);
     },
 };
+let ninja1Hp = ninja1.hp;
+let ninja2Hp = ninja2.hp;
+
+for (let i = 1; i <= 10; i++) {
+    console.log("===Round " + i + "===");
+    let ninja1Attack = ninja1.attack();
+    let ninja2Attack = ninja2.attack();
+    ninja2Hp -= ninja1Attack;
+    console.log(
+        "Ninja1 attacks Ninja2 and does a damage of " +
+            ninja1Attack +
+            "! Ninja1 health: " +
+            ninja1Hp +
+            ". Ninja2 health: " +
+            ninja2Hp
+    );
+    ninja1Hp -= ninja2Attack;
+    console.log(
+        "Ninja2 attacks Ninja1 and does a damage of " +
+            ninja2Attack +
+            "! Ninja1 health: " +
+            ninja1Hp +
+            ". Ninja2 health: " +
+            ninja2Hp
+    );
+}
+if (ninja1Hp > ninja2Hp) {
+}
